4911b9ab1eb40bb0493fd9d516bd428d3441379a
irvin <irvin.glez.real@hotmail.com>
2022-10-13 12:42:31 -0500

    Actualización de archivos

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-let cuentas = [
-    {nombre: "Juan", saldo: 850, contraseña: "5690" },
-    { nombre: "Laura", saldo: 390, contraseña: "3921" },
-    { nombre: "Rocío", saldo: 29, contraseña: "1082" },
-    ];
+// Variables --------------------------------------------
+let saldo5690 = [850];
+let saldo3921 = [290];
+let saldo1082 = [29];
 
+let cuenta5690 = { nombre: "Juan", saldo5690, contraseña: "5690" }
+let cuenta3921 = { nombre: "Laura", saldo3921, contraseña: "3921" }
+let cuenta1082 = { nombre: "Martha", saldo1082, contraseña: "1082" }
+
+const cuentaMartha = document.querySelector('#cuenta-martha')
+const cuentaLaura = document.querySelector('#cuenta-laura')
+const cuentaJuan = document.querySelector('#cuenta-juan')
+
+const btnSaldo = document.getElementById('btnsaldo')
+const btnIngresar = document.getElementById('btningresar')
+const btnRetirar = document.getElementById('btnretirar')
+const btnInRe = document.querySelector('#btnIngresarRetirar')
+const salirBtn = document.getElementById("salir");
+
+const seccionBienvenida = document.querySelector('.main-box__right')
+const seccionSaldo = document.querySelector('#seccion-saldo')
+const opciones = document.querySelector('.opciones')
+const opcion = document.querySelector('#opcion')
+let pantallaSaldo = document.getElementById('saldo-span')
+// let inputSaldoNuevo = document.getElementById('saldo-nuevo').value
+
+let saldoNuevo
+let saldoActual
+
+
+
+// ---------------------------------------------------------
+// ------------- Formulario de Entada ----------------------
 
     function login(form){
-        if ( form.u.value == "Juan" ) {
+        if ( form.u.value == `${cuenta5690.nombre}`) {
             if ( form.c.value == "5690" ) {
                 location = "principal5690.html"
             } else {
@@ -14,7 +41,7 @@ let cuentas = [
             } 
         }
 
-         else if ( form.u.value == "Laura" ) {
+         else if ( form.u.value == `${cuenta3921.nombre}` ) {
                 if ( form.c.value == "3921" ) {
                     location = "principal3921.html"
                 } else {
@@ -22,7 +49,7 @@ let cuentas = [
                 }
         }
 
-        else if ( form.u.value == "Martha" ) {
+        else if ( form.u.value == `${cuenta1082.nombre}` ) {
             if ( form.c.value == "1082" ) {
                 location = "principal1082.html"
             } else {
@@ -34,29 +61,153 @@ let cuentas = [
         }
     }
 
-    function clickSalir() {
-        console.log("El usario hizo click");
+// ------------- Formulario de Entada ----------------------
+// ---------------------------------------------------------
+
+
+
+// Eventos -------------------------------------------------
+
+btnSaldo.addEventListener('click', function() {
+
+    if (cuentaJuan){
+        let saldoNuevo = saldo5690[saldo5690.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    } else if (cuentaLaura){
+        let saldoNuevo = saldo3921[saldo3921.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    } else if (cuentaMartha){
+        let saldoNuevo = saldo1082[saldo1082.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
     }
 
-    const salirBtn = document.getElementById("salir");
-    salirBtn.addEventListener(click,clickSalir);  
+    seccionBienvenida.style.display = 'none'
+    seccionSaldo.style.display = 'block'
+    opciones.style.display = 'none'
+})
+
+btnIngresar.addEventListener('click', function(){
+
+    if (cuentaJuan){
+        let saldoNuevo = saldo5690[saldo5690.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    } else if (cuentaLaura){
+        let saldoNuevo = saldo3921[saldo3921.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    } else if (cuentaMartha){
+        let saldoNuevo = saldo1082[saldo1082.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    }
+
+    seccionBienvenida.style.display = 'none'
+    seccionSaldo.style.display = 'block'
+    opciones.style.display = 'block'
+    opcion.innerHTML = "Ingresar Cantidad:"
+    btnInRe.value = "Ingresar"
+    
+})
+
+btnRetirar.addEventListener('click', function(){
+    
+    if (cuentaJuan){
+        let saldoNuevo = saldo5690[saldo5690.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    } else if (cuentaLaura){
+        let saldoNuevo = saldo3921[saldo3921.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    } else if (cuentaMartha){
+        let saldoNuevo = saldo1082[saldo1082.length-1];
+        saldoActual = saldoNuevo
+        pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+    }
+
+    opcion.innerHTML = "Retitar Cantidad:"
+    btnInRe.value = "Retirar"
+    seccionBienvenida.style.display = 'none'
+    seccionSaldo.style.display = 'block'
+    opciones.style.display = 'block'
+
+})
+
+btnInRe.addEventListener('click', function(){   // ----------------------------------- btnInRe
+    let inputSaldoNuevo = document.getElementById('saldo-nuevo').value
+
+    if (btnInRe.value == "Ingresar"){   // ------------------------------------------- btnIngresar
+        if (cuentaJuan){
+            let saldoNuevo = saldo5690[saldo5690.length-1];
+            saldoActual = saldoNuevo + parseInt(inputSaldoNuevo)
+            pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+            saldo5690.push(saldoActual)
+            limpiar()
+        } else if (cuentaLaura){
+            let saldoNuevo = saldo3921[saldo3921.length-1];
+            saldoActual = saldoNuevo + parseInt(inputSaldoNuevo)
+            pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+            saldo3921.push(saldoActual)
+            limpiar()
+        } else if (cuentaMartha){
+            let saldoNuevo = saldo1082[saldo1082.length-1];
+            saldoActual = saldoNuevo + parseInt(inputSaldoNuevo)
+            pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+            saldo1082.push(saldoActual)
+            limpiar()
+        }
 
 
-// -----------------------------------------------
-// var factura = {
-//     numero: 201,
-//     cliente: 'Transportes Chemita',
-//     divisa: 'eur',
-//     subtotal: 350.25,
-//     IVA: 75.55,
-//     total: function(){
-//     return this.subtotal + this.IVA;
-//     }
-// }
+    } else if (btnInRe.value == "Retirar"){ // ---------------------------------------- btnRetirar
+        if (cuentaJuan){
+            if ( inputSaldoNuevo > saldo5690[saldo5690.length-1] ) {
+                alert('No cuentas con saldo suficiente')
+                limpiar()
+            } else {
+            let saldoNuevo = saldo5690[saldo5690.length-1];
+            saldoActual = saldoNuevo - parseInt(inputSaldoNuevo)
+            pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+            saldo5690.push(saldoActual)
+            limpiar()
+            }
+        } else if (cuentaLaura){
+            if ( inputSaldoNuevo > saldo3921[saldo3921.length-1] ) {
+                alert('No cuentas con saldo suficiente')
+                limpiar()
+            } else {
+            saldoNuevo = saldo3921[saldo3921.length-1];
+            saldoActual = saldoNuevo - parseInt(inputSaldoNuevo)
+            pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+            saldo3921.push(saldoActual)
+            limpiar()
+            }
+        } else if (cuentaMartha){
+            if ( inputSaldoNuevo > saldo1082[saldo1082.length-1] ) {
+                alert('No cuentas con saldo suficiente')
+                limpiar()
+            } else {
+            saldoNuevo = saldo1082[saldo1082.length-1];
+            saldoActual = saldoNuevo - parseInt(inputSaldoNuevo)
+            pantallaSaldo.innerHTML = `${"$ " + saldoActual + ".00"} `
+            saldo1082.push(saldoActual)
+            limpiar()
+            }
+        }
+    }
 
-// var numeroFactura = factura.numero;
-// var totalFactura = factura.total();
+})
 
-// console.log('La factura ' + numeroFactura + ' tiene un importe de ' + totalFactura);
 
-// La factura 201 tiene un importe de 425.8
+salirBtn.addEventListener('click', function(){  // --------------------------------------  Botón Salir
+    location = 'index.html'
+});
+
+
+function limpiar(){
+    let inputSaldoNuevo = document.getElementById('saldo-nuevo')
+    inputSaldoNuevo.value = ''
+}
